e978818b14130603c83ec9e733ff3d71e4d1f5ec
0230cp <rhwocks69@gmail.com>
2023-02-06 21:08:49 +0900

    2109
    solved

---
+++ Baekjoon/2109.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> pq;
+vector<pair<int,int>>v;
+int n,d,p,ret;
+
+int main(){
+	cin >> n;
+	
+	 for(int i = 0; i <n; i++){
+	 	cin >> p >> d;
+	 	v.push_back({d,p});
+	 }
+	 sort(v.begin(), v.end());
+	 
+	 for(int i = 0; i <n; i++){
+	 	pq.push(v[i].second);
+	 	if(pq.size() > v[i].first) pq.pop();
+	 }
+	 while(pq.size())ret += pq.top(), pq.pop();
+	 cout << ret << '\n';
+}
