70376c6d08c0376de25c16138f16a8fe449d5326
0230cp <rhwocks69@gmail.com>
2023-03-28 14:16:49 +0900

    11022
    solved

---
+++ Baekjoon/11022.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int A, B, t;
+int cnt = 1;
+
+void inputCount(){
+	cin >> t;
+}
+
+void input(){
+	cin >> A >> B;
+}
+
+void output(int a, int b){
+	cout << "Case #" << cnt << ": " << A << " + " << B << " = " << a + b << '\n';
+	cnt++;
+}
+
+int main(){
+	inputCount();
+	for(int i = 0; i < t; i++){
+		input();
+		output(A, B);
+	}
+}
