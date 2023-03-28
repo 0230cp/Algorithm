5d64ef208db4ed7b5a1c4f124d3e0955cf8bcd09
0230cp <rhwocks69@gmail.com>
2023-03-28 10:38:16 +0900

    1000
    solved

---
+++ Baekjoon/1000.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int A, B;
+
+void input(){
+	cin >> A >> B;
+}
+
+void output(int a, int b){
+	cout << a + b << '\n';
+}
+
+int main(){
+	input();
+	output(A, B);
+}
