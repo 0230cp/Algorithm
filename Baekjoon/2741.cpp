8ae2b1d7e901fba52e3b1c86c4d677f20284d405
0230cp <rhwocks69@gmail.com>
2023-03-29 14:28:35 +0900

    2741
    solved

---
+++ Baekjoon/2741.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+void input(){
+	cin >> n;
+}
+
+void sol(){
+	for(int i = 1; i <= n; i++){
+		cout << i << '\n';
+	}
+}
+
+int main(){
+	input();
+	sol();
+}
