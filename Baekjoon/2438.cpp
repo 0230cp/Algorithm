7cffe522502694a34ef46901cf095f0c91cb70a2
0230cp <rhwocks69@gmail.com>
2023-04-03 20:30:07 +0900

    2438
    solved

---
+++ Baekjoon/2438.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int input(){
+	int n;
+	cin >> n;
+	return n;
+}
+
+void sol(int num){
+	for(int i = 0; i < num; i++){
+		for(int j = 0; j <= i; j++){
+			cout << "*";
+		}
+		cout << '\n';
+	}
+}
+
+int main(){
+	sol(input());
+}
