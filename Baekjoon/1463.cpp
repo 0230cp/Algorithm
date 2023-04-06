b10b5f974f44b8707baeadbc12465b44ff6c753e
0230cp <rhwocks69@gmail.com>
2023-04-06 15:39:58 +0900

    1463
    solved

--- Baekjoon/1463.cpp
+++ Baekjoon/1463.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int dp[1000004];
+int input(){
+	int n;
+	cin >> n;
+	return n;
+}
 
-void solve(int n){
-	dp[1] = 0;
-	for(int i = 2; i <= n; i++){
+void sol(int n){
+	vector<int> dp(n+1);
+	dp[1] = 0, dp[2] = 1, dp[3] = 1;
+	for(int i = 4; i <= n; i++){
 		dp[i] = dp[i-1] + 1;
-		if(!(i%3)) dp[i] = min(dp[i], dp[i/3] + 1);
-		if(!(i%2)) dp[i] = min(dp[i], dp[i/2] + 1);
+		if(i % 2 == 0) dp[i] = min(dp[i/2] + 1, dp[i]);
+		if(i % 3 == 0) dp[i] = min(dp[i/3] + 1, dp[i]);
 	}
 	cout << dp[n] << '\n';
 }
 
 int main(){
-	cin >> n;
-	solve(n);
+	sol(input());
 }
