b51213c855eb534ed1199d50c36cf7c27f445faf
0230cp <rhwocks69@gmail.com>
2023-04-07 23:53:58 +0900

    2193
    changed

--- Baekjoon/2193.cpp
+++ Baekjoon/2193.cpp
@@ -8,12 +8,11 @@ int input(){
 }
 
 void sol(int n){
-	int dp[1004];
+	long long dp[94];
 	dp[1] = 1;
-	dp[2] = 3;
+	dp[2] = 1;
 	for(int i = 3; i <= n; i++){
-		dp[i] = dp[i-1] + dp[i-2]*2;
-		dp[i] = dp[i] % 10007;
+		dp[i] = dp[i-1] + dp[i-2];
 	}
 	cout << dp[n] << '\n';
 }
