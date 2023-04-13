66e66d996c506d7baaca987588d138420a1321d3
0230cp <rhwocks69@gmail.com>
2023-04-13 21:44:16 +0900

    1699
    solved

--- Baekjoon/1699.cpp
+++ Baekjoon/1699.cpp
@@ -1,24 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
+int n;
 
-    vector<int> dp(n+1);
-
-    for (int i = 1; i <= n; i++) {
-        dp[i] = i;
-        for (int j = 1; j*j <= i; j++) {
-            dp[i] = min(dp[i], dp[i - j*j] + 1);
-        }
-    }
-
-    cout << dp[n] << endl;
-
-    return 0;
+void input(){
+	cin >> n;
 }
 
+void sol(int num){
+	int dp[100004];
+	
+	for(int i = 1; i <= num; i++){
+		dp[i] = i;
+		for(int j = 1; j * j <= i; j++){
+			dp[i] = min(dp[i], dp[i - j*j] + 1);
+		}
+	}
+	cout << dp[num] << '\n';
+}
+
+int main(){
+	input();
+	sol(n);
+}
