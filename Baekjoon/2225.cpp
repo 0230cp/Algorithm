f978d104e71272fcc0a8fb4722cdefe32e37c6bb
0230cp <rhwocks69@gmail.com>
2023-04-21 00:25:54 +0900

    2225
    solved

--- Baekjoon/2225.cpp
+++ Baekjoon/2225.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 using namespace std;
 
+int n,k;
 const int MOD = 1000000000;
 
 int dp[201][201]; // dp[i][j] : j를 i개의 정수로 나타내는 방법의 수
 
+void input(){
+	cin >> n >> k;
+}
+
+void sol(){
+	for(int i = 0; i <= k; i++){
+		dp[1][i] = i;
+	}
+	
+	for(int i = 1; i <= n; i++){
+		dp[i][1] = 1;
+	}
+	
+	for(int i = 2; i <= n; i++){
+		for(int j = 2; j <= k; j++){
+			dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % MOD;
+		}
+	}
+	cout << dp[n][k] << '\n';
+}
 int main() {
-    int n, k;
-    cin >> n >> k;
-
-    // 초기화
-    for (int i = 0; i <= n; i++) {
-        dp[1][i] = 1; // 1을 i개의 정수로 나타내는 방법은 모두 1개
-        dp[2][i] = i + 1; // 2를 i개의 정수로 나타내는 방법은 i+1개
-    }
-
-    // dp 계산
-    for (int i = 3; i <= k; i++) {
-        for (int j = 0; j <= n; j++) {
-            for (int l = 0; l <= j; l++) {
-                dp[i][j] += dp[i-1][j-l];
-                dp[i][j] %= MOD;
-            }
-        }
-    }
-
-    cout << dp[k][n] << endl;
-
-    return 0;
+    input();
+    sol();
 }
 
