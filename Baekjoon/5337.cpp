66355355cc4095e1722f32c3d1d2cd204402b4aa
0230cp <rhwocks69@gmail.com>
2023-04-24 00:18:52 +0900

    5337
    solved

--- Baekjoon/5337.cpp
+++ Baekjoon/5337.cpp
@@ -1,8 +1,11 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-	cout << ".  .   ." << '\n';
-	cout << "|  | _ | _. _ ._ _  _" << '\n';
-	cout << "|/\|(/.|(_.(_)[ | )(/." << '\n';
+ 
+int main()
+{
+	cout << ".  .   ." << endl;
+	cout << "|  | _ | _. _ ._ _  _" << endl;
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+ 
+	return 0;
 }
