a95b6fe72b7359efde9396fff2539d82a16e5cc3
0230cp <rhwocks69@gmail.com>
2023-05-23 23:24:50 +0900

    9086
    solved

--- Baekjoon/9085.cpp
+++ Baekjoon/9085.cpp
@@ -1,20 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
-
 int main() {
-	int c, n, num, sum = 0;
-	cin >> c;
-	for (int i = 0; i < c; i++) {
-		cin >> n;
-		for (int j = 0; j < n; j++) {
-			cin >> num;
-			sum += num;
-		}
-		cout << sum << '\n';
-		sum = 0;
-	}
-
-	return 0;
+    int n;
+    string k;
+    cin >> n;
+    while (n--)
+    {
+        cin >> k;
+        cout << k[0] << k[k.size() - 1] <<'\n';
+    }
 }
-
-
