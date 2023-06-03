46e35abfe2dcbfc5cfa3e29ebd09b588d353151c
0230cp <rhwocks69@gmail.com>
2023-06-04 01:27:38 +0900

    11286
    solved

--- Baekjoon/11286.cpp
+++ Baekjoon/11286.cpp
@@ -3,35 +3,40 @@
 #include <cmath>
 using namespace std;
 
+int n;
+
 struct Compare {
     bool operator()(int a, int b) {
-        if (abs(a) == abs(b))
+        if (abs(a) == abs(b)) {
             return a > b;
-        return abs(a) > abs(b);
+        } else {
+            return abs(a) > abs(b);
+        }
     }
 };
 
-int main() {
-    int N;
-    cin >> N;
-
-    priority_queue<int, vector<int>, Compare> pq;
-    for (int i = 0; i < N; i++) {
-        int x;
-        cin >> x;
-        
-        if (x == 0) {
-            if (pq.empty())
-                cout << "0\n";
-            else {
-                cout << pq.top() << "\n";
-                pq.pop();
-            }
-        } else {
-            pq.push(x);
-        }
-    }
-
-    return 0;
+void sol(){
+	int a;
+	priority_queue<int, vector<int>, Compare> pq;
+	for(int i = 0; i < n; i++){
+		cin >> a;
+		
+		if(a == 0){
+			if(pq.empty()) cout << "0" << '\n';
+			else{
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}else{
+			pq.push(a);
+		}
+	}
 }
 
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n;
+	sol();
+}
